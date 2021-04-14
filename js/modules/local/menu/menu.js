1bdeb19a0483b59d74e4fb6aab3b12995f6200f9
Alisson Cipriano <58789035+AlissonCiprian0@users.noreply.github.com>
2021-04-14 17:42:58 -0300

    Catalog and Product pages partially finished (80%)
    Product cards moved from showcases at home to a dedicated product_card Element.
    
    This Element is used both in the showcases and in the Catalog page.
    
    Product page has a product gallery and comments section enabled.
    It also has a product hanking (5 stars avaliation).
    
    All this pages are only working in mobile screens at the moment (usually max of 768px, but some work ok at 7024px as well).

--- js/modules/local/menu/menu.js
+++ js/modules/local/menu/menu.js
@@ -21,11 +21,51 @@ jQuery('#login-button').click(function(event) {
 // MR - Menu Retrátil
 let ulCategoriasMR = jQuery('#categorias_header-m');
 let botaoToggleSubcategoriaMR = jQuery('#categorias_header-m > li.father > button');
+let botaoToggleSubcategoria_2MR = jQuery('#categorias_header-m > li.father > div.children > div.ct_child > button');
 let subcategoriasMR = jQuery('#categorias_header-m > li.father > div.children');
+let subcategorias_2MR = jQuery('#categorias_header-m > li.father > div.children > div.ct_child > div.children_child');
 
 subcategoriasMR.slideUp();
+subcategorias_2MR.slideUp();
 
 botaoToggleSubcategoriaMR.on('click', function(e) {
-    jQuery(e.target).parent().find('div.children').slideToggle();
-    jQuery(this).text('-')
+    // jQuery(e.target).parent().find('div.children').slideToggle("fast", "swing");
+
+    if(jQuery(this).hasClass('open')) {
+        jQuery(this).text('+');
+        jQuery(this).removeClass('open');
+        jQuery(e.target).parent().find('div.children').slideUp("fast", "swing");
+    }
+    else {
+        subcategoriasMR.slideUp();
+        botaoToggleSubcategoriaMR.removeClass('open');
+        botaoToggleSubcategoriaMR.text('+');
+
+        subcategorias_2MR.slideUp();
+        botaoToggleSubcategoria_2MR.removeClass('open');
+        botaoToggleSubcategoria_2MR.text('+');
+
+        jQuery(this).text('-');
+        jQuery(this).addClass('open');
+        jQuery(e.target).parent().find('div.children').slideDown("fast", "swing");
+    }
+});
+
+botaoToggleSubcategoria_2MR.on('click', function(e) {
+    // jQuery(e.target).parent().find('div.children_child').slideToggle("fast", "swing");
+
+    if(jQuery(this).hasClass('open')) {
+        jQuery(this).text('+');
+        jQuery(this).removeClass('open');
+        jQuery(e.target).parent().find('div.children_child').slideUp("fast", "swing");
+    }
+    else {
+        subcategorias_2MR.slideUp();
+        botaoToggleSubcategoria_2MR.removeClass('open');
+        botaoToggleSubcategoria_2MR.text('+');
+
+        jQuery(this).text('-');
+        jQuery(this).addClass('open');
+        jQuery(e.target).parent().find('div.children_child').slideDown("fast", "swing");
+    }
 });
