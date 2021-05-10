329d44e93c96346c93aa9e2855600ac37af0db38
Alisson <alisson.cipriano@outlook.com>
2021-05-10 14:55:23 -0300

    SuperCoffee v1
    All responsive adaptions made.
    SuperCoffee header version finished.
    
    Todas as adaptações de responsividade foram feitas.
    Versão SuperCoffee do menu finalizada.

--- js/modules/local/product_page/galeriaSuperCoffee.js
+++ js/modules/local/product_page/galeriaSuperCoffee.js
@@ -1,6 +1,19 @@
+// Checar mobile
+if (jQuery("#isMobile").attr('data-is-mobile') == 'true') {
+    var mobile = true;
+}
+else {
+    var mobile = false;
+}
+
 // Gerar galeia ao carregar a p�gina
 function carregaGaleria() {
-    lightGallery(document.getElementById('ul-lightgallery'));
+    if (mobile) {
+        lightGallery(document.getElementById('ul-lightgallery'));
+    }
+    else {
+        lightGallery(document.getElementById('galeria_supercoffee-d'));
+    }
 }
 
 carregaGaleria();
